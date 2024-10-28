5a4abb50b3bc56cce3dcd5aa1ed9e547ec8fc6cf
Craftinators <craftinatorssx@gmail.com>
2024-10-28 15:15:15 -0400

    init

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+// prettier.config.mjs
+export default {
+    semi: true,                // Adds semicolons at the end of statements
+    singleQuote: false,         // Uses single quotes instead of double quotes
+    trailingComma: 'all',      // Adds trailing commas where valid in ES5 (objects, arrays, etc.)
+    tabWidth: 2,               // Sets indentation to 2 spaces
+    useTabs: false,            // Uses spaces for indentation instead of tabs
+    printWidth: 80,            // Wraps lines at 80 characters
+    bracketSpacing: true,      // Adds spaces between brackets in object literals
+    endOfLine: 'lf',           // Uses LF for line endings
+};
